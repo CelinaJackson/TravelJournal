bb5e166329ce0967b66b29f00728c7edf5394ddb
CelinaJackson <celinanjackson@gmail.com>
2020-09-17 13:17:34 -0400

    Create ApplicationController

--- Controllers/application_controller.rb
+++ Controllers/application_controller.rb
@@ -1,1 +1,14 @@
 
+class ApplicationController < Sinatra::Base 
+    
+    configure do 
+        set :public_foler, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "traveljournal"
+    end
+    
+    get '/' do 
+        "Hello World!"
+    end 
+end 
