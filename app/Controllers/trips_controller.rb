92be590492662d5ffa1efa9a088ee9cbfd651d2e
CelinaJackson <celinanjackson@gmail.com>
2020-09-21 13:29:33 -0400

    editing

--- app/Controllers/trips_controller.rb
+++ app/Controllers/trips_controller.rb
@@ -2,7 +2,7 @@ class TripsController < ApplicationController
 
     get "/trips" do
       redirect_if_not_logged_in
-      @trips = Trips.all
+      @trips = Trip.all
       erb :'trips/index'
     end
   
@@ -15,14 +15,14 @@ class TripsController < ApplicationController
     get "/trips/:id/edit" do
       redirect_if_not_logged_in
       @error_message = params[:error]
-      @trip = Trips.find(params[:id])
+      @trip = Trip.find(params[:id])
       erb :'trips/edit'
     end
   
     post "/trips/:id" do
       redirect_if_not_logged_in
-      @trip = Trips.find(params[:id])
-      unless Trips.valid_params?(params)
+      @trip = Trip.find(params[:id])
+      unless Trip.valid_params?(params)
         redirect "/trips/#{@trip.id}/edit?error=invalid trip"
       end
       @trip.update(params.select{|k|k=="name" || k=="capacity"})
@@ -31,17 +31,17 @@ class TripsController < ApplicationController
   
     get "/trips/:id" do
       redirect_if_not_logged_in
-      @trip = Trips.find(params[:id])
+      @trip = Trip.find(params[:id])
       erb :'trips/show'
     end
   
     post "/trips" do
       redirect_if_not_logged_in
   
-      unless Trips.valid_params?(params)
+      unless Trip.valid_params?(params)
         redirect "/trips/new?error=invalid trip"
       end
-      Trips.create(params)
+      Trip.create(params)
       redirect "/trips"
     end
   end
