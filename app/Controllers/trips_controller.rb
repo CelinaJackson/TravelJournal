23573efcf4a03249af250d6693533fc14a6bc3a2
CelinaJackson <celinanjackson@gmail.com>
2020-09-23 16:50:45 -0400

    syntax changes

--- app/Controllers/trips_controller.rb
+++ app/Controllers/trips_controller.rb
@@ -1,48 +1,48 @@
 class TripsController < ApplicationController 
 
-    get "/trips" do
+    get '/trips' do
       redirect_if_not_logged_in
       @trips = Trip.all
       erb :'/trips/index'
     end
   
-    get "/trips/new.html" do
+    get '/trips/new.html' do
       redirect_if_not_logged_in
       @error_message = params[:error]
       erb :'/trips/new.html'
     end
   
-    get "/trips/:id/edit" do
+    get '/trips/:id/edit' do
       redirect_if_not_logged_in
       @error_message = params[:error]
       @trip = Trip.find(params[:id])
       erb :'/trips/edit.html'
     end
   
-    post "/trips/:id" do
+    post '/trips/:id' do
       redirect_if_not_logged_in
       @trip = Trip.find(params[:id])
       unless Trip.valid_params?(params)
-        redirect "/trips/#{@trip.id}/edit?error=invalid trip"
+        redirect '/trips/#{@trip.id}/edit?error=invalid trip'
       end
       @trip.update(params.select{|k|k=="name" || k=="capacity"})
-      redirect "/trips/#{@trip.id}"
+      redirect '/trips/#{@trip.id}'
     end
   
-    get "/trips/:id" do
+    get '/trips/:id' do
       redirect_if_not_logged_in
       @trip = Trip.find(params[:id])
       erb :'/trips/show.html'
     end
   
-    post "/trips" do
+    post '/trips' do
       redirect_if_not_logged_in
   
       unless Trip.valid_params?(params)
-        redirect "/trips/new?error=invalid trip"
+        redirect '/trips/new?error=invalid trip'
       end
       Trip.create(params)
-      redirect "/trips"
+      redirect '/trips'
     end
   end
   
