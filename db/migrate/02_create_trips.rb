ec2a43734eb940c858f6cb71277a2fb80ac89339
CelinaJackson <celinanjackson@gmail.com>
2020-09-28 14:24:11 -0400

    deleted

--- db/migrate/02_create_trips.rb
+++
@@ -1,9 +0,0 @@
-class CreateTrips < ActiveRecord::Migration
-    def change 
-        create_table :trips do |t|
-            t.string :location
-            t.string :description 
-            t.integer :user_id 
-        end 
-    end 
-end 
--- db/migrate/03_create_saved_trips.rb
+++
@@ -1,6 +0,0 @@
-class CreateSavedLessons < ActiveRecord::Migration 
-  def change 
-    create_table :saved_trips do |t|
-        t.integer :saving_user_id
-  end 
-end 
