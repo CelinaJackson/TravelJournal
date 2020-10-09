b69ee7c62407639f57593e778e40d1268a802d55
CelinaJackson <celinanjackson@gmail.com>
2020-10-09 10:35:53 -0400

    deleted duplicate files

--- SinatraProject/app/Models/trips.rb
+++
@@ -1,5 +0,0 @@
-class Trip < ActiveRecord::Base 
-  validates_presence_of :location, :description
-
-  belongs_to :user
-end 
--- SinatraProject/app/Models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-    has_secure_password
-    validates_presence_of :name, :email
-
-    validates :email, :presence => true, :uniqueness => true, :format => {:with => /\w+@\w+\.\w+/}
-    
-    has_many :trips
- 
-end 
