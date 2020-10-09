e68bfdf70df0e90017fed25be800baf0fd417176
CelinaJackson <celinanjackson@gmail.com>
2020-10-09 10:37:59 -0400

    deleted duplicate files

--- SinatraProject/SinatraProject/config.ru
+++
@@ -1,7 +0,0 @@
-require_relative './config/environment'
- 
-
-use Rack::MethodOverride
-use TripsController
-use UsersController
-run ApplicationController 
--- SinatraProject/SinatraProject/db/migrate/20200928181052_create_saved_trips.rb
+++
@@ -1,7 +0,0 @@
-class CreateSavedTrips < ActiveRecord::Migration[6.0]
-  def change
-    create_table :saved_trips do |t|
-      t.integer :saving_user_id
-    end
-  end
-end
